eae277fb148ce5eb3f61e3bad90b82a32998727f
Shahbaz Mahmood <shahbaz.mahmood@seqera.io>
2025-07-04 12:37:23 -0400

    removing members from overlay

--- examples/hello-world-aws/main.tf
+++ examples/hello-world-aws/main.tf
@@ -194,12 +194,35 @@ resource "seqera_teams" "my_teams" {
 }
 
 ## Launch a workflow
-
-
 resource "seqera_workflows" "my_workflows" {
   compute_env_id = resource.seqera_compute_env.aws_batch_compute_env.compute_env_id
   pipeline             = "nextflow-io/hello"
   work_dir     = local.work_dir
   workspace_id = resource.seqera_workspace.my_workspace.id
-  depends_on = [ seqera_compute_env.aws_batch_compute_env ]
+  depends_on = [ seqera_compute_env.aws_batch_compute_env]
+
 }
+
+
+## Data Studi
+
+resource "seqera_data_studios" "my_datastudios" {
+  compute_env_id        = resource.seqera_compute_env.aws_batch_compute_env.compute_env_id
+  description           = "Data studio"
+  name                  = "Terraform-Data-Studio"
+  configuration = {
+    #conda_environment = ""
+    # cpu               = 6
+    # gpu               = 8
+    # lifespan_hours    = 5
+    # memory            = 9
+    # mount_data = [
+    #   "..."
+    # ]
+  }
+  workspace_id          = resource.seqera_workspace.my_workspace.id
+  data_studio_tool_url = "public.cr.seqera.io/platform/data-studio-jupyter:4.2.5-0.8"
+  depends_on = [ seqera_compute_env.aws_batch_compute_env]
+
+}
+
