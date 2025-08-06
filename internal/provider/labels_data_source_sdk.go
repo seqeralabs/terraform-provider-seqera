9b8161932f8bce2fe93e42dbb4e4804e5683b7e0
Shahbaz Mahmood <39392909+Shahbaz-mahmood123@users.noreply.github.com>
2025-08-06 18:56:10 -0400

    missing changes (#46)

--- internal/provider/labels_data_source_sdk.go
+++ internal/provider/labels_data_source_sdk.go
@@ -17,27 +17,18 @@ func (r *LabelsDataSourceModel) RefreshFromSharedListLabelsResponse(ctx context.
 
 	if resp != nil {
 		r.Labels = []tfTypes.LabelDbDto{}
-		if len(r.Labels) > len(resp.Labels) {
-			r.Labels = r.Labels[:len(resp.Labels)]
-		}
-		for labelsCount, labelsItem := range resp.Labels {
+
+		for _, labelsItem := range resp.Labels {
 			var labels tfTypes.LabelDbDto
+
 			labels.DateCreated = types.StringPointerValue(typeconvert.TimePointerToStringPointer(labelsItem.DateCreated))
 			labels.ID = types.Int64PointerValue(labelsItem.ID)
 			labels.IsDefault = types.BoolPointerValue(labelsItem.IsDefault)
 			labels.Name = types.StringPointerValue(labelsItem.Name)
 			labels.Resource = types.BoolPointerValue(labelsItem.Resource)
 			labels.Value = types.StringPointerValue(labelsItem.Value)
-			if labelsCount+1 > len(r.Labels) {
-				r.Labels = append(r.Labels, labels)
-			} else {
-				r.Labels[labelsCount].DateCreated = labels.DateCreated
-				r.Labels[labelsCount].ID = labels.ID
-				r.Labels[labelsCount].IsDefault = labels.IsDefault
-				r.Labels[labelsCount].Name = labels.Name
-				r.Labels[labelsCount].Resource = labels.Resource
-				r.Labels[labelsCount].Value = labels.Value
-			}
+
+			r.Labels = append(r.Labels, labels)
 		}
 		r.TotalSize = types.Int64PointerValue(resp.TotalSize)
 	}
