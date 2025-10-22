153e7773051dfe0b5e152196ecb7c9c15c63526c
Gavin <gav.elder@gmail.com>
2025-10-22 17:43:35 +0100

    Ge/feat/managed credentials (#91)
    * feat: managed identities
    
    * Managed Creds
    
    * Google Credentials Validation

--- examples/resources/seqera_google_credential/resource.tf
+++ examples/resources/seqera_google_credential/resource.tf
@@ -1,13 +1,25 @@
 resource "seqera_google_credential" "my_googlecredential" {
-  base_url       = "...my_base_url..."
-  category       = "...my_category..."
-  checked        = false
-  credentials_id = "...my_credentials_id..."
-  description    = "...my_description..."
-  keys = {
-    data = "{\n  \"type\": \"service_account\",\n  \"project_id\": \"my-project\",\n  \"private_key_id\": \"key-id\",\n  \"private_key\": \"-----BEGIN PRIVATE KEY-----\\n...\\n-----END PRIVATE KEY-----\\n\",\n  \"client_email\": \"service-account@my-project.iam.gserviceaccount.com\",\n  \"client_id\": \"123456789\",\n  \"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\",\n  \"token_uri\": \"https://oauth2.googleapis.com/token\"\n}\n"
-  }
-  name          = "...my_name..."
+  name          = "My Google Credentials"
+  description   = "Google Cloud service account for workflow execution"
   provider_type = "google"
-  workspace_id  = 6
+  workspace_id  = 123
+
+  # Recommended: Use file() function to read the service account key JSON
+  keys = {
+    data = file("${path.module}/service-account-key.json")
+  }
+
+  # Alternative: Inline JSON (not recommended for production)
+  # keys = {
+  #   data = jsonencode({
+  #     type         = "service_account"
+  #     project_id   = "my-project"
+  #     private_key_id = "key-id"
+  #     private_key  = "-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----\n"
+  #     client_email = "service-account@my-project.iam.gserviceaccount.com"
+  #     client_id    = "123456789"
+  #     auth_uri     = "https://accounts.google.com/o/oauth2/auth"
+  #     token_uri    = "https://oauth2.googleapis.com/token"
+  #   })
+  # }
 }
