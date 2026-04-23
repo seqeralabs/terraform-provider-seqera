822b0e76b810df15aaa092fca3d0ff30d97e2d4a
Stefano Boriero <stefano.boriero@seqera.io>
2026-04-23 15:26:18 +0200

    fix!: mark workspace date fields as readonly
    Mark dateCreated and lastUpdated fields of workspaces as read only with
    a speakeasy overlay. While the Platform API accepts a payload containing
    these values, they are ignored by Seqera Platform and are automatically
    replaced. This would cause constant drifting in terraform if specified.
    Technically this is a breaking change, because projects supplying values
    for those fields will fail at the plan phase. Migration would be simply
    removing those fields form the resource definition.

--- examples/resources/seqera_workspace/resource.tf
+++ examples/resources/seqera_workspace/resource.tf
@@ -1,10 +1,8 @@
 resource "seqera_workspace" "my_workspace" {
-  date_created = "2022-12-28T06:42:48.248Z"
-  description  = "Workspace for genomics research projects and computational biology workflows"
-  full_name    = "Genomics Research Workspace"
-  id           = 1
-  last_updated = "2021-06-30T04:32:09.681Z"
-  name         = "genomics-research"
-  org_id       = 7
-  visibility   = "SHARED"
+  description = "Workspace for genomics research projects and computational biology workflows"
+  full_name   = "Genomics Research Workspace"
+  id          = 1
+  name        = "genomics-research"
+  org_id      = 7
+  visibility  = "SHARED"
 }
